c7d220b217b9415f36186023ab25d5355298348a
xyurt <yurtrimu@gmail.com>
2026-06-03 07:02:34 +0300

    Update pool.c

--- pool.c
+++ pool.c
@@ -15,7 +15,7 @@
 
 static void object_pool_offsets_init(struct object_pool *pool);
 
-int object_pool_init(struct object_pool *pool, size_t count, size_t object_size)
+int object_pool_init(struct object_pool *pool, size_t capacity, size_t object_size)
 {
 	size_t struct_size = sizeof(size_t) + object_size;
 
@@ -23,14 +23,14 @@ int object_pool_init(struct object_pool *pool, size_t count, size_t object_size)
 		struct_size = ALIGN(struct_size);
 	}
 
-	if (!pool || count == 0 || object_size == 0)
+	if (!pool || capacity == 0 || object_size == 0)
 		return -EINVAL;
 
-	pool->block = OBJECT_POOL_MALLOC(count * struct_size);
+	pool->block = OBJECT_POOL_MALLOC(capacity * struct_size);
 	if (!pool->block)
 		return -ENOMEM;
 
-	pool->count = count;
+	pool->capacity = capacity;
 	pool->struct_size = struct_size;
 
 	object_pool_offsets_init(pool);
@@ -49,7 +49,7 @@ int object_pool_cleanup(struct object_pool *pool)
 	pool->block = NULL;
 	pool->head = 0;
 	
-	pool->count = 0;
+	pool->capacity = 0;
 	pool->struct_size = 0;
 	pool->object_size = 0;
 
@@ -78,7 +78,7 @@ void *object_pool_acquire(struct object_pool *pool)
 	pool->head = *(size_t *)target;
 	*(size_t *)target = head;
 
-	return target + sizeof(size_t); /* result is the object, excluding the header */
+	return target + sizeof(size_t);
 }
 
 int object_pool_release(struct object_pool *pool, void *object) 
@@ -99,19 +99,27 @@ int object_pool_release(struct object_pool *pool, void *object)
 	return 0;
 }
 
+void *object_pool_at(struct object_pool *pool, size_t idx) 
+{
+	if (!pool || idx >= pool->capacity)
+		return NULL;
+
+	return pool->block + idx * pool->struct_size + sizeof(size_t);
+}
+
 static void object_pool_offsets_init(struct object_pool *pool)
 {
 	size_t i = 0;
-	size_t count = pool->count;
+	size_t capacity = pool->capacity;
 	char *block = pool->block;
 	size_t struct_size = pool->struct_size;
 	size_t tmp;
 
 	for (;;) {
-		if (i == count)
+		if (i == capacity)
 			break;
 
-		if (i + 1 == count) {
+		if (i + 1 == capacity) {
 			tmp = 0;
 		}
 		else {

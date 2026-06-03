3fc97a3516772f55d2b5914d678c12b9d582db87
xyurt <yurtrimu@gmail.com>
2026-06-03 07:02:25 +0300

    Update pool.h

--- pool.h
+++ pool.h
@@ -13,7 +13,7 @@
 
 /* 
 * 
-* Use OBJECT_POOL_NO_ALIGN definition to disable alignments. 
+* Use OBJECT_POOL_NO_ALIGN definition to disable alignments.
 * This may reduce performance or even be undefined behaviour on some architectures. 
 * 
 */
@@ -21,7 +21,7 @@
 struct object_pool {
 	char *block;
 	size_t head;
-	size_t count;
+	size_t capacity;
 	size_t struct_size;
 	size_t object_size;
 };
@@ -38,7 +38,7 @@ typedef struct object_pool object_pool_t;
  *
  * Return: Returns 0 on success. If arguments are invalid returns -EINVAL. If malloc fails, returns -ENOMEM.
  */
-int object_pool_init(struct object_pool *pool, size_t count, size_t object_size);
+int object_pool_init(struct object_pool *pool, size_t capacity, size_t object_size);
 
 /**
  * object_pool_cleanup(struct object_pool *pool) - Cleans up the object pool structure
@@ -55,9 +55,10 @@ int object_pool_cleanup(struct object_pool *pool);
  * object_pool_acquire(struct object_pool *pool) - Acquires an object from the object pool
  * @pool: Pointer of the pool structure. Must not be NULL.
  *
- * Acquires an object from the object pool.
+ * Acquires an object from the object pool. 
+ * The object must not be free'd, pool still owns it.
  *
- * Return: Returns the object acquired, on success. If the pool is exhausted, arguments are invalid or if an error occurs returns NULL.
+ * Return: Returns the object acquired, on success. If the pool is exhausted or arguments are invalid then returns NULL.
  */
 void *object_pool_acquire(struct object_pool *pool);
 
@@ -72,5 +73,18 @@ void *object_pool_acquire(struct object_pool *pool);
  * Return: Returns 0 on success. If arguments are invalid returns -EINVAL.
  */
 int object_pool_release(struct object_pool *pool, void *object);
+
+/**
+ * object_pool_at(struct object_pool *pool, size_t idx) - Retrieves the object at index
+ * @pool: Pointer of the pool structure. Must not be NULL.
+ * @idx: Index of the object. Must not be more than or equal to capacity.
+ *
+ * Retrieves the object at index, idx. The ownership of the object is pool's. 
+ * The object must not be free'd.
+ * 
+ *
+ * Return: Returns the object retrieved, on success. If arguments are invalid then returns NULL.
+ */
+void *object_pool_at(struct object_pool *pool, size_t idx);
 
 #endif /* _OBJECT_POOL_H */
